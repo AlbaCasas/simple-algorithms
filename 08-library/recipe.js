a06330ebb15547a29be6adde2b35df263b77b576
Alba Casas <albacasasarzua1b@gmail.com>
2021-12-28 11:43:54 +0100

    Solve library problem

---
+++ 08-library/recipe.js
@@ -0,0 +1,48 @@
+
+/**
+ * Create a library app to manage your books. This app will ask the user to choose from a list of options upon starting:
+ * - Add a book (add)
+ * - View collection (view)
+ * - Exit (exit)
+ * 
+ * If the user inputs "add" your app should ask him to write different properties of the book he is adding, 
+ * these properties will be "title", "author" and "numberOfPages". Once the user enters all three, the book will be added to his collection.
+ * 
+ * If the user inputs "view", your app should show him a list of the TITLES of his books.
+ * 
+ * If the user inputs "exit", your app should tell him "Bye!" and exit.
+ * 
+ * Example
+ * -------
+ * 
+ * LIBRARY APP: 
+ * Choose an option
+ * - Add a book (add)
+ * - View collection (view)
+ * - Exit (exit)
+ * 
+ * :add
+ * 
+ * "Title of your book:" LOTR
+ * "Author": Tolkien
+ * "Number of pages": 1000
+ * "Book added!"
+ * 
+ * Choose an option
+ * - Add a book (add)
+ * - View collection (view)
+ * - Exit (exit)
+ * 
+ * :view
+ * 
+ * [LOTR]
+ * 
+ * Choose an option
+ * - Add a book (add)
+ * - View collection (view)
+ * - Exit (exit)
+ * 
+ * :exit
+ * 
+ */
+
